65d62868fbae437ea1ca7453922c574aedd0574f
unknown <lucasalexanddre@hotmail.com>
2022-01-16 20:15:51 -0300

    Try Except - trataiva de erros com raise

---
+++ try_except_raise.py
@@ -0,0 +1,62 @@
+# TRY EXCEPT - RAISE
+
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Raise an error with raise
+    if echo < 0:
+        raise ValueError("echo must be greater than or equal to 0")
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+# Call shout_echo
+shout_echo("particle", echo=5)
+
+#-------------------------------
+
+import pandas as pd
+
+tweets_df = pd.read_csv("C:/Users/lucasMartins/Documents/DataCamp/toolbox/Funções/tweets.csv")
+
+# Define count_entries()
+def count_entries(df, col_name='lang'):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+    
+    # Raise a ValueError if col_name is NOT in DataFrame
+    if col_name not in df.columns:
+        raise ValueError('The DataFrame does not have a ' + col_name + ' column.')
+
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+    
+    # Extract column from DataFrame: col
+    col = df[col_name]
+    
+    # Iterate over the column in DataFrame
+    for entry in col:
+
+        # If entry is in cols_count, add 1
+        if entry in cols_count.keys():
+            cols_count[entry] += 1
+            # Else add the entry to cols_count, set the value to 1
+        else:
+            cols_count[entry] = 1
+        
+        # Return the cols_count dictionary
+    return cols_count
+
+# Call count_entries(): result1
+result1 = count_entries(tweets_df, "lang")
+
+# Print result1
+print(result1)
