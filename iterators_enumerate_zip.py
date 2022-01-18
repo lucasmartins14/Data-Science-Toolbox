c322a70696125fff9c1349bf60c6dc77daee8a1a
unknown <lucasalexanddre@hotmail.com>
2022-01-18 00:37:37 -0300

    Iterar, enumerar e zipar objetos

---
+++ iterators_enumerate_zip.py
@@ -0,0 +1,64 @@
+# Iterators, enumerate e zip
+
+# Iterators
+# Create a range object: values
+values = range(10,21)
+
+# Print the range object
+print(values)
+
+# Create a list of integers: values_list
+values_list = list(values)
+
+# Print values_list
+print(values_list)
+
+# Get the sum of values: values_sum
+values_sum = sum(values)
+
+# Print values_sum
+print(values_sum)
+
+# Enumerate
+# Create a list of strings: mutants
+mutants = ['charles xavier', 
+            'bobby drake', 
+            'kurt wagner', 
+            'max eisenhardt', 
+            'kitty pryde']
+
+
+# Create a list of tuples: mutant_list
+mutant_list = list(enumerate(mutants))
+
+# Print the list of tuples
+print(mutant_list)
+
+# Unpack and print the tuple pairs
+for index1,value1 in enumerate(mutants):
+    print(index1, value1)
+
+# Change the start index
+for index2,value2 in enumerate(mutants, start=1):
+    print(index2, value2)
+
+# ZIP
+
+powers = ['telepathy', 'thermokinesis', 'teleportation', 'magnetokinesis', 'intangibility']
+
+# Create a zip object from mutants and powers: z1
+z1 = zip(mutants,powers)
+
+# Print the tuples in z1 by unpacking with *
+print(*z1)
+
+# Re-create a zip object from mutants and powers: z1
+z1 = zip(mutants,powers)
+
+# 'Unzip' the tuples in z1 by unpacking with * and zip(): result1, result2
+result1, result2 = zip(*z1)
+
+# Check if unpacked tuples are equivalent to original tuples
+print(result1 == mutants)
+print(result2 == powers)
+
