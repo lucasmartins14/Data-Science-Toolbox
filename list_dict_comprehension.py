f58531829d8cab7ef78226fd31f8ec047c0f69b0
unknown <lucasalexanddre@hotmail.com>
2022-01-19 01:41:33 -0300

    Compreensões de Listas e Dicionarios

---
+++ list_dict_comprehension.py
@@ -0,0 +1,40 @@
+# List and Dict Comprehension
+
+# Basic
+squares = [i**2 for i in range(0,10)]
+
+# Advanced
+
+# Using 1 conditional in comprehensions
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create list comprehension: new_fellowship
+new_fellowship = [member for member in fellowship if len(member) >= 7]
+
+# Print the new list
+print(new_fellowship)
+
+# Using 2 conditionals in comprehensions
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create list comprehension: new_fellowship
+new_fellowship = [member if len(member) >= 7 else "" for member in fellowship]
+
+# Print the new list
+print(new_fellowship)
+
+
+# Dict comprehension
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create dict comprehension: new_fellowship
+new_fellowship = {member: len(member) for member in fellowship}
+
+# Print the new dictionary
+print(new_fellowship)
